09719517ad3f7674b96501ccc682364c86b55243
Manuel Di Francesco <manueldifrancesco@Manuels-MacBook-Pro.local>
2025-05-19 23:33:01 -0300

    Arreglo rutas: assets con %BASE_URL%, entry script sin base

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/gestion-campo/',
   plugins: [react()],
   resolve: {
     alias: {
