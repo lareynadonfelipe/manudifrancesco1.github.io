6ae6bd4589115262bd6133cf755149c762518eaa
Manuel Di Francesco <manueldifrancesco@Manuels-MacBook-Pro.local>
2025-05-19 23:36:29 -0300

    Arreglo script entry y rutas de assets para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   base: '/gestion-campo/',
   plugins: [react()],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+    alias: { '@': path.resolve(__dirname, './src') },
   },
-});
+})
