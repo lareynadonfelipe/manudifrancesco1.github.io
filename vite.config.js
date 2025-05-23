6193e187d01ed96d6d61713d019c4efa7015cec8
Manuel Di Francesco <manueldifrancesco@Manuels-MacBook-Pro.local>
2025-05-23 15:57:10 -0300

    “Cambio“

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/gestion-campo/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
