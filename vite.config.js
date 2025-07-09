e0f4bad312894353a1bddeba075b1adaa9d9319a
Manuel Di Francesco <manueldifrancesco@Manuels-MacBook-Pro.local>
2025-07-09 12:45:35 -0300

    chore: ajustar vite.config para assets en docs

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,19 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/',
+  // rutas relativas: busca CSS, assets y scripts en la misma carpeta de index.html
+  base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
-  
     }
   },
   build: {
-    outDir: 'docs'
+    outDir: 'docs',
+    // vuela tus assets (css, imágenes) al root de docs/
+    assetsDir: '',
+    // limpia docs/ antes de generar
+    emptyOutDir: true
   },
   plugins: [react()]
 })
